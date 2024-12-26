209dbf86dd6199fa6131d0b1a454e9c0ee50260a
Oleksiiska <fxdha7483@gmail.com>
2024-12-26 20:33:49 +0200

    practice12

---
+++ practice-12/pr12_version_2.py
@@ -0,0 +1,61 @@
+from functools import reduce
+
+dirs = [
+    ( 'folder1',
+        [
+            'file1',
+            ( 'folder2', 
+                [
+                    'file2',
+                    'file3'
+                ] 
+            ),
+            ( 'folder3', 
+                [
+                    'file3', 
+                    'file4',
+                    ('folder4', ['file3'])
+                ] 
+            ),
+            'file5'
+        ]
+    )
+]
+
+# Code (with accumulator)
+
+def search(dirs, filename, acc=""):
+    if type(dirs) == str:
+        return [f'{acc}{filename}'] if dirs == filename else []
+    
+    if type(dirs) == tuple:
+        folder, cont = dirs
+        result = search(cont, filename, f'{acc}/{folder}/')
+        return result
+    
+    if type(dirs) == list and len(dirs) == 0:
+        return []
+    
+    if type(dirs) == list and len(dirs) > 0:
+        result = map(lambda item: search(item, filename, acc), dirs)
+        return reduce(lambda a, b: a + b, result, [])
+    
+
+# Test
+
+print(search(dirs, 'file1'))
+print(search(dirs, 'file2'))
+print(search(dirs, 'file3'))
+print(search(dirs, 'file4'))
+print(search(dirs, 'file5'))
+print(search(dirs, 'file6'))
+print(search(dirs, 'folder1'))
+
+assert search(dirs, 'file1') == ['/folder1/file1'], 'Failed test for file1'
+assert search(dirs, 'file2') == ['/folder1//folder2/file2'], 'Failed test for file2'
+assert search(dirs, 'file3') == ['/folder1//folder2/file3', '/folder1//folder3/file3', '/folder1//folder3//folder4/file3'], 'Failed test for file3'
+assert search(dirs, 'file4') == ['/folder1//folder3/file4'], 'Failed test for file4'
+assert search(dirs, 'file5') == ['/folder1/file5'], 'Failed test for file5'
+assert search(dirs, 'file6') == [], 'Failed test for file6'
+assert search(dirs, 'folder1') == [], 'Failed test for folder1'
+print('All tests good!')
